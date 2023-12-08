fa282614bab00e3a6e772009e891b45d3390ed18
bora <nhatbaoxxd@gmail.com>
2023-12-08 09:14:30 +0700

    implement AppProvider

--- src/App-before.js
+++ src/App-before.js
@@ -1,9 +1,34 @@
-import { useContext, useEffect, useState } from 'react'
-import AppProvider, { AppContext } from './AppContext'
-import { createRandomPost } from './helper'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker'
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  }
+}
+
+// 1. CREATE A CONTEXT
+const AppContext = createContext()
 
 export default function App() {
+  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()))
+  const [searchQuery, setSearchQuery] = useState('')
   const [isFakeDark, setIsFakeDark] = useState(false)
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) => `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase()))
+      : posts
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts])
+  }
+
+  function handleClearPosts() {
+    setPosts([])
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(() => {
@@ -11,18 +36,26 @@ export default function App() {
   }, [isFakeDark])
 
   return (
-    <section>
-      <button onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)} className="btn-fake-dark-mode">
-        {isFakeDark ? '☀️' : '🌙'}
-      </button>
-
-      <AppProvider>
+    // 2. PROVIDE VALUE TO CHILD COMPONENTS
+    <AppContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
+        <button onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)} className="btn-fake-dark-mode">
+          {isFakeDark ? '☀️' : '🌙'}
+        </button>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </AppProvider>
-    </section>
+      </section>
+    </AppContext.Provider>
   )
 }
 
